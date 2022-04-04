8e479568eba2d86e709acae4edd4c4d91f9f39ad
vikhang2t1 <vikhangngo953@gmail.com>
2022-04-04 21:07:39 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import AppHeader from '../src/components/AppHeader.vue'
+</script>
+
+<template>
+  <div id="app">
+        <AppHeader />
+        <div class="container mt-3">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<style>
+.page {
+    max-width: 400px;
+    margin: auto;
+}
+</style>
