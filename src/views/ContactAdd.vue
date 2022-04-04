7df4106bab660aaec74a4a1bf8ca088e926dc4a9
vikhang2t1 <vikhangngo953@gmail.com>
2022-04-04 21:40:21 +0700

    update file ContactAdd

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,37 @@
+<template>
+        <h4>Thêm liên hệ mới</h4>
+        <ContactFormAdd 
+            @submit:contact="createContact" 
+            
+        />
+        <p>{{ message }}</p>
+</template>
+
+<script>
+    import ContactService from "../services/contact.service"; 
+    import ContactFormAdd from "../components/ContactFormAdd.vue";
+    export default {
+        name: "ContactAdd",
+        components: { 
+            ContactFormAdd, 
+        },
+        data() { 
+            return { 
+                message: "", 
+            }; 
+        },
+        methods: {
+            async createContact(data) {
+                console.log(data)
+                try{
+                    await ContactService.create(data);
+                    this.message = "Đã thêm liên hệ mới thành công.";
+                }
+                catch(err){
+                    console.log(err);
+                }
+            },
+            
+        },
+    }
+</script>
